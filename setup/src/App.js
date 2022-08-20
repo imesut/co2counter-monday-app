74620f4d6384740ec8a53b8ad4f41b9f56f848b8
Mesut Yılmaz <im.mesut.yilmaz@gmail.com>
2022-08-20 21:21:50 +0300

    added draft UI for setup steps and board selection

--- setup/src/App.js
+++ setup/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import mondaySdk from "monday-sdk-js";
 import "monday-ui-react-core/dist/main.css"
 //Explore more Monday React Components here: https://style.monday.com/
-import { Heading, MultiStepIndicator, Box, Flex, Dropdown } from "monday-ui-react-core"
+import { Heading, MultiStepIndicator, Box, Flex, Dropdown, Divider, Button, Steps } from "monday-ui-react-core"
 
 const monday = mondaySdk();
 
@@ -32,20 +32,20 @@ class App extends React.Component {
   render() {
 
     let steps = [{
-      key: "FULFILLED",
-      status: MultiStepIndicator.stepStatuses.FULFILLED,
-      titleText: "1 - Setup your Expense",
-      subtitleText: "Define CO Emission Sources for your business"
+      key: "STEP1",
+      status: MultiStepIndicator.stepStatuses.ACTIVE,
+      titleText: "1 - Expenses",
+      subtitleText: ""
     }, {
-      key: "PENDING",
+      key: "STEP2",
       status: MultiStepIndicator.stepStatuses.PENDING,
-      titleText: "2 - Define Carbon Neutralization Strategy",
-      subtitleText: "What you'll do to neutralize emissions?"
+      titleText: "2 - Carbon Neutralization",
+      subtitleText: ""
     }, {
-      key: "PENDING-3",
+      key: "STEP3",
       status: MultiStepIndicator.stepStatuses.PENDING,
-      titleText: "3 - Approval",
-      subtitleText: "See your first score and perform changes if necessary."
+      titleText: "3 - Approve",
+      subtitleText: ""
     }];
 
 
@@ -71,34 +71,68 @@ class App extends React.Component {
       flexDirection: "column"
     }}>
      
-
-     <Heading type={Heading.types.h1} value="Welcome to Carbon Tracker" />
-     <MultiStepIndicator className="monday-storybook-multiStepIndicator_big-size" steps={steps} textPlacement={MultiStepIndicator.textPlacements.VERTICAL} />
-
+     <Flex direction={Flex.directions.COLUMN}>
+        <Heading type={Heading.types.h1} value="Welcome to Carbon Tracker" />
+        <p>Set up your carbon policy by following our pre-built schema or by customizing for your own needs.</p>
+        <MultiStepIndicator className="monday-storybook-multiStepIndicator_big-size" steps={steps} textPlacement={MultiStepIndicator.textPlacements.VERTICAL} />
+    </Flex>
       <Box className="boxWrapper" shadow={Box.shadows.MEDIUM} border={Box.borders.DEFAULT} rounded={Box.roundeds.MEDIUM} backgroundColor={Box.backgroundColors.GREY_BACKGROUND_COLOR}>
-      
-      <Flex direction={Flex.directions.COLUMN}>
+        
+      <Flex justify={Flex.justify.SPACE_AROUND}>
 
-        <Heading type={Heading.types.h2} size="small" value="A. Choose Expense" />
-        <p>Choose a Table which contains your expense records.</p>
-        <Dropdown
-            className="dropdown-stories-styles_spacing carbon-dropdown"
-            onChange={function noRefCheck(){}}
-            onClear={function noRefCheck(){}}
-            onInputChange={function noRefCheck(){}}
-            onOptionRemove={function noRefCheck(){}}
-            onOptionSelect={function noRefCheck(){}}
-            options={tablesForExpenseRecords}
-            placeholder="Placeholder text here"
-        />
+        <Flex justify={Flex.justify.START} direction={Flex.directions.COLUMN}>
+            <Heading type={Heading.types.h2} size="small" value="Create Prebuilt Schema" />
+            <Button onClick={function noRefCheck(){}}>
+            Create
+            </Button>
+            
 
 
+        <div style={{ width: '600px' }}>
+            <Flex>
+            <Divider /><span style={{padding: 20, width: 300}}>or Customize</span><Divider />
+            </Flex>
+        </div>
 
+            <Heading type={Heading.types.h3} size="small" value="First, Choose Expense" />
+            <p>Choose a Table which contains your expense records.</p>
+            <Dropdown
+                className="dropdown-stories-styles_spacing carbon-dropdown"
+                onChange={function noRefCheck(){}}
+                onClear={function noRefCheck(){}}
+                onInputChange={function noRefCheck(){}}
+                onOptionRemove={function noRefCheck(){}}
+                onOptionSelect={function noRefCheck(){}}
+                options={tablesForExpenseRecords}
+                placeholder="Placeholder text here"
+            />
+
+            
+
+            <Heading type={Heading.types.h3} size="small" value="Then, Pick your Carbon Calculator" />
+            <p>Choose a Table to calculate your expenses' carbon footprint.</p>
+            <Dropdown
+                className="dropdown-stories-styles_spacing carbon-dropdown"
+                onChange={function noRefCheck(){}}
+                onClear={function noRefCheck(){}}
+                onInputChange={function noRefCheck(){}}
+                onOptionRemove={function noRefCheck(){}}
+                onOptionSelect={function noRefCheck(){}}
+                options={tablesForExpenseRecords}
+                placeholder="Placeholder text here"
+            />
         </Flex>
-      </Box>
+    </Flex>
+
+    <div style={{bottom: 0, position: "absolute", width: "inherit", padding: 8, backgroundColor: "#f6f7fb"}}>
+        <Divider />
+        <Steps activeStepIndex={0} steps={[<div/>,<div/>,<div/> ]} />
+
+    </div>
+
+    </Box>
 
 
-  
     </div>
   }
 }
