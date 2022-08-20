fa0781ccab1ded41f8f460184f718b497b7ef06d
Mesut Yılmaz <im.mesut.yilmaz@gmail.com>
2022-08-20 16:50:27 +0300

    Inited text based widget

--- quickstart-react/src/App.js
+++ quickstart-react/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import mondaySdk from "monday-sdk-js";
 import "monday-ui-react-core/dist/main.css"
 //Explore more Monday React Components here: https://style.monday.com/
-import AttentionBox from "monday-ui-react-core/dist/AttentionBox.js"
+import { Heading, LinearProgressBar, Tooltip } from "monday-ui-react-core"
 
 const monday = mondaySdk();
 
@@ -21,10 +21,10 @@ class App extends React.Component {
   componentDidMount() {
     // TODO: set up event listeners
 
-    monday.api(`query { me { name } }`).then((res) => {
-      this.setState({ name: res.data.me.name });
-      console.log(res.data.me.name)
-    });
+    // monday.api(`query { me { name } }`).then((res) => {
+    //   this.setState({ name: res.data.me.name });
+    //   console.log(res.data.me.name)
+    // });
 
   }
 
@@ -32,12 +32,66 @@ class App extends React.Component {
 
     let text = "Let's start building your amazing app, which will change the world! " + this.state.name
 
-    return <div className="App">
-      <AttentionBox
-        title="Hello Monday Apps!"
-        text={text}
-        type="success"
-      />
+    let data = {
+      this_year: {
+        emission: 20,
+        emission_target: 50,
+        neutralization: 5,
+        neutralization_target: 10,
+        net_target: 15
+      },
+      year_2023: {
+        emission_target: 40,
+        neutralization_target: 20,
+        net_target: 20
+      },
+      year_2024: {
+        emission_target: 20,
+        neutralization_target: 20,
+        net_target: 0
+      }
+    }
+
+    let emission_rate = data.this_year.emission / data.this_year.emission_target * 100;
+    let neutralization_rate = data.this_year.neutralization / data.this_year.neutralization_target * 100;
+    let net_target_rate = data.this_year.neutralization / data.this_year.net_target * 100;
+    let month = 8;
+    let OnTrackProgress = month / 12 * 100;
+
+    return <div className="App" style={{
+      display: "flex",
+      flexDirection: "column"
+    }}>
+     
+
+     <Heading type={Heading.types.h2} value="Carbon Policy Overview" size="small" />
+    <p>Being carbon neutral until <b>2025</b></p>
+
+    <Heading type={Heading.types.h3} value="Net Carbon Status" size="small" />
+    <p><b>{data.this_year.emission - data.this_year.neutralization}</b> kg-CO2 (net) produced. This is <b>{ Math.round(net_target_rate) }%</b> of the net carbon emission for this year. </p>
+
+
+
+    <Heading type={Heading.types.h3} value="Carbon Emissions" size="small" />
+    <div className="linear-progress-bar_column fwidth">
+        <div className="linear-progress-bar_block">
+          <LinearProgressBar className="linear-progress-bar_small-wrapper" indicateProgress value={emission_rate} valueSecondary={OnTrackProgress} size={LinearProgressBar.sizes.LARGE} />
+          {data.this_year.emission} kg-CO2 emission
+        </div>  
+      </div>
+
+      <Heading type={Heading.types.h3} value="Neutralized Carbon Emissions" size="small" />
+    <div className="linear-progress-bar_column fwidth">
+        <div className="linear-progress-bar_block">
+          <LinearProgressBar className="linear-progress-bar_small-wrapper" indicateProgress value={neutralization_rate} valueSecondary={OnTrackProgress} size={LinearProgressBar.sizes.LARGE} />
+          {data.this_year.emission} kg-CO2 emission saved
+        </div>  
+      </div>
+
+
+
+
+
     </div>;
   }
 }
